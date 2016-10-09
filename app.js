d195d4a8b12382d85c294129bb597e77fd8f771c
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2016-10-09 02:09:46 -0400

    removed twilio credentials from app

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// Twilio Credentials
-var accountSid = 'ACa9301c80392577ceab046d878520a077';
-var authToken = 'd1597410f93e2250dac631341f43f542';
-
 // App modules
 var express = require('express');
 var app = express();
