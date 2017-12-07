94f7a644ccbb312b23f71a6ba36ea3d257202936
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-12-07 17:29:45 -0500

    Updated with es6 features

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
 // App modules
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var open = require('open');
+const express = require('express');
+let app = express();
+const bodyParser = require('body-parser');
+const open = require('open');
 
 // Youtube modules && Key
-var YouTube = require('youtube-node');
-var youTube = new YouTube();
+const YouTube = require('youtube-node');
+let youTube = new YouTube();
 youTube.setKey('AIzaSyCfNYilt9yW_HaD5pGcp-JIRPwKdTt2ANQ');
 
 // App
  app.use(bodyParser.urlencoded({ extended: false }));
 
 //Gets message
-app.post('/message', function(req, res) {
-  console.log(req.body);
-  var msgFrom = req.body.From;
+app.post('/message', (req, res) => {
+  // Will get the song
+  const msgFrom = req.body.From;
   msgBody = req.body.Body;
   res.send("<Response> <Message>NOW PLAYING: " + msgBody + "</Message> </Response>");
 
-  youTube.search(msgBody, 2, function(error, result) {
+  // Search for the song
+  youTube.search(msgBody, 2, (error, result) => {
     if (error) {
       console.log(error);
-    }
-    else {
+
+    } else {
       // Retrieve data from api
-      var json = JSON.stringify(result, null, 2);
-      var obj = JSON.parse(json);
-      var id = obj["items"][0]["id"]["videoId"];
+      let json = JSON.stringify(result, null, 2);
+      let obj = JSON.parse(json);
+      let id = obj["items"][0]["id"]["videoId"];
 
       // console.log(obj);
-      open("https://www.youtube.com/watch?v=" + id);
+      open(`https://www.youtube.com/watch?v=${id}`);
     }
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+
+  console.log('SMS: (866) 754-0963');
+});
